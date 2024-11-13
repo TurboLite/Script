1e33fe823aff58be52891f60c64a389614bfb207
TurboLite <168972472+TurboLite@users.noreply.github.com>
2024-11-13 16:24:44 +0700

    Add files via upload

--- FixLag.lua
+++ FixLag.lua
@@ -1,3 +1,21 @@
+repeat
+    wait()
+until game:IsLoaded()
+if game.PlaceId == 2753915549 then
+    World1 = true
+elseif game.PlaceId == 4442272183 then
+    World2 = true
+elseif game.PlaceId == 7449423635 then
+    World3 = true
+end
+game.StarterGui:SetCore(
+    "SendNotification",
+    {
+        Title = "Đang Tải",
+        Text = "Hãy Đợi Chút Nha",
+        Duration = 3
+    })
+
 local function FPSBooster()
     local decalsyeeted = true
     local g = game
