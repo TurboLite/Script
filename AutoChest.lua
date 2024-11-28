5d0c2dfc6b0518cb8ad4e89df19a37a035ce41d7
TurboLite <168972472+TurboLite@users.noreply.github.com>
2024-11-28 23:00:03 +0700

    Add files via upload

--- AutoChest.lua
+++ AutoChest.lua
@@ -1,7 +1,26 @@
+
 -- Thông Báo 
 require(game.ReplicatedStorage:WaitForChild("Notification")).new(
             " <Color=Green>Turbo Lite — Auto Chest<Color=/> "
         ):Display()
+        
+-- Join 
+-- Kiểm tra xem giao diện chọn đội có tồn tại không
+if game:GetService("Players").LocalPlayer.PlayerGui.Main:FindFirstChild("ChooseTeam") then
+    -- Chọn đội "Pirates" ngay lập tức
+    local chooseTeamGui = game:GetService("Players").LocalPlayer.PlayerGui.Main.ChooseTeam
+    local piratesButton = chooseTeamGui.Container["Pirates"].Frame.TextButton
+
+    -- Kích hoạt sự kiện khi người chơi chọn đội "Pirates"
+    for _, connection in pairs(getconnections(piratesButton.Activated)) do
+        -- Giả lập việc nhấn nút
+        for _, inputConnection in pairs(getconnections(game:GetService("UserInputService").TouchTapInWorld)) do
+            inputConnection:Fire() 
+        end
+        -- Thực thi hành động liên kết với nút
+        connection.Function()
+    end
+end
         
 --Webhook
 function PostWebhook(Url, message)
