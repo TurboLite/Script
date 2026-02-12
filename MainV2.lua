1f363aa92afaabfae275e1a3be33ab4180ce1f1a
TurboLite <168972472+TurboLite@users.noreply.github.com>
2026-02-12 09:57:36 +0700

    Add files via upload

--- MainV2.lua
+++ MainV2.lua
@@ -682,7 +682,7 @@ BringEnemy = function()
             local dist = (root.Position - targetPos).Magnitude  
 
             if dist <= _G.BringRange and not root:GetAttribute("Tweening") then  
-                count += 2  
+                count += 3  
                 root:SetAttribute("Tweening", true)  
 
                 local tween = TweenService:Create(  
