845047a7050bc4056f600f0b0aae4e0a2b2fd082
TurboLite <168972472+TurboLite@users.noreply.github.com>
2026-04-18 19:13:20 +0700

    Update HopServer.lua

--- HopServer.lua
+++ HopServer.lua
@@ -1,3 +1,3 @@
-script_key = "e8cn84w9jvdm77lrsrgj4lo2"
+script_key = "ex5ldgl8i764uc3l8u6hmw94"
 
 loadstring(game:HttpGet("https://api.getpolsec.com/scripts/hosted/2be6c0ffc14980b51ee4fa5ea55b67d56bda4430e466524797c1c503b4b8c336.lua"))()
